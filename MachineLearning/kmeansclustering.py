f28b85c13b71402881ae512d5cd4d3fdc0d87334
netra2127 <netra200021kcbdr@gmail.com>
2024-05-24 18:51:20 +0100

    kmeans clustering

---
+++ MachineLearning/kmeansclustering.py
@@ -0,0 +1,69 @@
+"""
+Types of ML Learning:
+********************************
+1. Supervised:
+********************************
+-> Input and output or labels. 
+Contains:
+Regression: Label --> Numerical.
+Classification: Label --> Categorical. 
+
+********************************
+2. Unsupervised:
+********************************
+-> Input without Labels. 
+Clustering. 
+Association. 
+Dimensionality Reduction. 
+Anomoly detection. 
+
+********************************
+3. Reinforcement:
+********************************
+
+********************************
+********************************
+#. K means Clustering:
+********************************
+********************************
+Problem statement:
+1. Decide k clusters. 
+2. Initialize Centroids. 
+3. Start Iterations. 
+Assign clusters. 
+Move centroid. 
+Check and stop. 
+
+#. How do we decide correct value of k ?
+--> Elbow method --> works on the principle of the inertia. 
+
+# Limitation of Elbow Methods:
+1. Subjectivity in Identifying the Elbow. 
+2. Not suitable for all Datasets.
+3. Performance with large number of features. 
+4. Does not consider cluster quality. 
+5. Sensitivity of scaling. 
+
+# Assumptions of K-Means:
+1. Spherical Cluster shape. 
+2. Similar Cluster shape. 
+3. Equal Variance of cluster. 
+4. Clusters are well separated. 
+5. Number of Clusters(k) is pre-defined. 
+6. Large n, Small K. 
+
+
+# Limitation of K-Means:
+1. Vulnerability to Outliers. 
+2. Hard Clustering. 
+3. High-Dimensional Challenge. 
+4. Sensitive to scale. 
+
+Note: Euclidean distance is not reliable at high dimensional data-points. 
+"""
+
+
+
+
+
+
